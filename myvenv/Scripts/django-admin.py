4ec19725d67cf42ab8bcef41b19d4aead0a874fe
mcoldanghise <mcoldanghise@unomaha.edu>
2020-04-14 15:09:59 -0500

    Added restapi feature

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mark\Desktop\ISQA3900\Assignments\Assignment2_foodservices\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
